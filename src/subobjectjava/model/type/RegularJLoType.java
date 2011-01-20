2c39d2e034ccb4ffcda1906f5ab00df967134b95
marko <marko@raven>
2011-01-20 21:34:07 +0100

    initial import

---
+++ src/subobjectjava/model/type/RegularJLoType.java
@@ -0,0 +1,35 @@
+package subobjectjava.model.type;
+
+import java.util.List;
+
+import jnome.core.type.RegularJavaType;
+import subobjectjava.model.component.ComponentRelation;
+import chameleon.core.declaration.SimpleNameSignature;
+import chameleon.oo.type.inheritance.InheritanceRelation;
+
+public class RegularJLoType extends RegularJavaType {
+
+	public RegularJLoType(SimpleNameSignature sig) {
+		super(sig);
+	}
+
+	public RegularJLoType(String name) {
+		super(name);
+	}
+
+	@Override
+	public List<InheritanceRelation> inheritanceRelations() {
+		// first take the subtype relations
+		List<InheritanceRelation> result = super.inheritanceRelations();
+		// then add the component relations
+		List<ComponentRelation> components = directlyDeclaredElements(ComponentRelation.class);
+		result.addAll(components);
+		return result;
+	}
+	
+	public List<InheritanceRelation> nonMemberInheritanceRelations() {
+		return super.inheritanceRelations();
+	}
+
+
+}
