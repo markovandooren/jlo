375cace33f5fad32670b1da7396c54fff517920c
Marko van Dooren <Marko.vanDooren@cs.kuleuven.be>
2012-11-15 17:00:08 +0100

    working on writing the project config file

--- src/jlo/model/type/RegularJLoType.java
+++ src/jlo/model/type/RegularJLoType.java
@@ -2,11 +2,15 @@ package jlo.model.type;
 
 import java.util.List;
 
+import org.rejuse.logic.ternary.Ternary;
+
 import jlo.model.component.ComponentRelation;
 import jlo.model.component.Overrides;
 import jnome.core.type.RegularJavaType;
 import chameleon.core.declaration.SimpleNameSignature;
 import chameleon.core.lookup.LookupException;
+import chameleon.core.property.ChameleonProperty;
+import chameleon.oo.language.ObjectOrientedLanguage;
 import chameleon.oo.member.Member;
 import chameleon.oo.member.MemberRelationSelector;
 import chameleon.oo.type.Type;
@@ -42,6 +46,19 @@ public class RegularJLoType extends RegularJavaType {
 		super(name);
 	}
 	
+//	@Override
+//	public synchronized Ternary is(ChameleonProperty property) {
+//		if(property == language(ObjectOrientedLanguage.class).OVERRIDABLE) {
+//			System.out.println("debug");
+//		}
+//		Ternary result = super.is(property);
+//		if(property == language(ObjectOrientedLanguage.class).OVERRIDABLE && result == Ternary.UNKNOWN) {
+//			flushLocalCache();
+//			System.out.println("debug");
+//		}
+//		return result;
+//	}
+
 	public List<InheritanceRelation> inheritanceRelations() throws LookupException {
 		// first take the subtype relations
 		List<InheritanceRelation> result = super.inheritanceRelations();
