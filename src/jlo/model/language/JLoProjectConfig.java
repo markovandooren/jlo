b13712a58ad676efac51a74e0b6e89c2dfc34cc5
Marko van Dooren <Marko.vanDooren@cs.kuleuven.be>
2013-01-10 15:38:42 +0100

    update

--- src/jlo/model/language/JLoProjectConfig.java
+++ src/jlo/model/language/JLoProjectConfig.java
@@ -1,6 +1,7 @@
 package jlo.model.language;
 
 import java.io.File;
+import java.net.URL;
 import java.util.Collections;
 import java.util.List;
 
@@ -14,6 +15,11 @@ public class JLoProjectConfig extends JavaProjectConfig {
 	public JLoProjectConfig(View view, FileInputSourceFactory inputSourceFactory, String projectName, File root, String baseJarPath)
 			throws ConfigException {
 		super(view, inputSourceFactory, projectName, root, baseJarPath);
+		// The base loader for Java already creates the primitive types
+		// and load the Java base library.
+		// Therefore, we only have to add a loader for the base JLo library file.
+		URL url = getClass().getProtectionDomain().getCodeSource().getLocation();
+		System.out.println(url);
 		// No BaseJLoProjectLoader is created (doesn't exist) since there no additional predefined elements yet.
 	}
 	
