07a17f7479f3bbea85c790911c7210ecbc80f5b7
Marko van Dooren <Marko.vanDooren@ugent.be>
2015-05-07 00:29:36 +0200

    Documentation and simplification

--- src/org/aikodi/jlo/model/type/ParenthesisTypeReference.java
+++ src/org/aikodi/jlo/model/type/ParenthesisTypeReference.java
@@ -1,40 +1,53 @@
 package org.aikodi.jlo.model.type;
 
-import org.aikodi.chameleon.core.declaration.Declaration;
 import org.aikodi.chameleon.core.element.Element;
 import org.aikodi.chameleon.core.element.ElementImpl;
-import org.aikodi.chameleon.core.lookup.LookupContext;
 import org.aikodi.chameleon.core.lookup.LookupException;
 import org.aikodi.chameleon.oo.type.Type;
 import org.aikodi.chameleon.oo.type.TypeReference;
 import org.aikodi.chameleon.util.association.Single;
 
+/**
+ * <p>A type reference that wraps another type reference in parentheses.
+ * This language construct is used for keyword-style type expressions.
+ * Instead of writing <code>List&lt;T&gt;</code> or <code>Map&lt;T&gt;</p>
+ * 
+ * @author Marko van Dooren
+ */
 public class ParenthesisTypeReference extends ElementImpl implements TypeReference {
 
 	private Single<TypeReference> _typeReference = new Single<>(this);
-	
-	@Override
-	public Declaration getDeclarator() throws LookupException {
-		return typeReference().getDeclarator();
+
+	/**
+	 * Create a new parenthesis type reference that wraps the given type reference
+	 * 
+	 * @param wrappedReference The type reference to be wrapped.
+	 */
+	public ParenthesisTypeReference(TypeReference wrappedReference) {
+	  set(_typeReference, wrappedReference);
 	}
 	
-	public TypeReference typeReference() {
+	/**
+	 * @return The type reference that is wrapped by this parenthesis type
+	 * reference.
+	 */
+	public TypeReference wrappedReference() {
 		return _typeReference.getOtherEnd();
 	}
 
-	@Override
-	public LookupContext targetContext() throws LookupException {
-		return getElement().targetContext();
-	}
-
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @return The type referenced by the wrapped type reference.
+	 */
 	@Override
 	public Type getElement() throws LookupException {
-		return typeReference().getElement();
+		return wrappedReference().getElement();
 	}
 
 	@Override
 	protected Element cloneSelf() {
-		return new ParenthesisTypeReference();
+		return new ParenthesisTypeReference(null);
 	}
 
 }
