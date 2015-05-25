47e4f1919674561a35d6b2ce1a22fbc6664cebf1
Marko van Dooren <Marko.van.Dooren@gmail.com>
2015-05-25 21:19:06 +0200

    Added constraint

--- src/org/aikodi/jlo/model/type/TypeMemberDeclarator.java
+++ src/org/aikodi/jlo/model/type/TypeMemberDeclarator.java
@@ -2,7 +2,6 @@ package org.aikodi.jlo.model.type;
 
 import java.util.List;
 
-import org.aikodi.chameleon.core.declaration.Declaration;
 import org.aikodi.chameleon.core.declaration.Signature;
 import org.aikodi.chameleon.core.declaration.SimpleNameSignature;
 import org.aikodi.chameleon.core.element.Element;
@@ -12,7 +11,9 @@ import org.aikodi.chameleon.core.modifier.ElementWithModifiersImpl;
 import org.aikodi.chameleon.oo.member.Member;
 import org.aikodi.chameleon.oo.type.Type;
 import org.aikodi.chameleon.oo.type.TypeFixer;
+import org.aikodi.chameleon.oo.type.generics.EqualityConstraint;
 import org.aikodi.chameleon.oo.type.generics.FormalTypeParameter;
+import org.aikodi.chameleon.oo.type.generics.TypeConstraint;
 import org.aikodi.chameleon.oo.type.generics.TypeParameter;
 import org.aikodi.chameleon.oo.type.generics.TypeParameterFixer;
 import org.aikodi.chameleon.util.Lists;
@@ -83,8 +84,11 @@ public class TypeMemberDeclarator extends ElementWithModifiersImpl implements Me
 		result.setUniParent(fixer);
 		return result;
 	}
+	
 	private FormalTypeParameter createParameter() {
-		return new FormalTypeParameter(signature().name());
+		FormalTypeParameter formalTypeParameter = new FormalTypeParameter(signature().name());
+		formalTypeParameter.addConstraint(clone(constraint()));
+		return formalTypeParameter;
 	}
 
 	@Override
@@ -99,6 +103,16 @@ public class TypeMemberDeclarator extends ElementWithModifiersImpl implements Me
 
 	private Single<Signature> _signature = new Single<>(this);
 
+	private Single<TypeConstraint> _constraint = new Single<>(this);
+
+	public TypeConstraint constraint() {
+		return _constraint.getOtherEnd();
+	}
+	
+	public void setConstraint(TypeConstraint constraint) {
+		set(_constraint, constraint);
+	}
+	
 	@Override
 	protected Element cloneSelf() {
 		return new TypeMemberDeclarator((SimpleNameSignature)null);
